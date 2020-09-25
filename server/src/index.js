89ebf3b9414820cb6d290e21f143833e7a4f36f8
EddyPBR <edvaldojunior1310@gmail.com>
2020-09-25 15:59:08 -0300

    Server now save the log of the loads

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const oracledb = require("oracledb");
+const fs = require("fs");
 const cors = require("cors");
 const app = express();
 const port = 3333;
@@ -39,10 +40,28 @@ async function selectAllLoadsById(req, res) {
 
 app.use(cors());
 
+app.use(express.json());
+
 app.get("/loads/:id", function (req, res) {
   selectAllLoadsById(req, res);
+});
+
+app.post("/save", function (req, res) {
+  const data = req.body;
+
+  const fileName = new Date().toJSON();
+
+  fs.writeFile(`./src/logs/${fileName}.json`, JSON.stringify(data), function(error) {
+    if(error) {
+      console.log("ERROR TO SAVE LOG!!!");
+      return res.status(500).send("System error!");
+    } else {
+      return res.status(200).send("Sucess");
+    }
+  });
 });
 
 app.listen(port, () =>
   console.log("nodeOracleRestApi app listening on port %s!", port)
 );
+
